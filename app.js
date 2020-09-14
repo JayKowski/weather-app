615504d36a8e4b785b1a9dac32b8844487ff2982
JayKowski <jaymaina5@gmail.org>
2020-09-15 01:06:18 +0300

    changed some line arrangements

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // eslint-disable-next-line
 const express = require('express');
+const app = express();
 
 // eslint-disable-next-line linebreak-style
 const path = require('path');
 
 const port = process.env.PORT || 8080;
-const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(`${__dirname}/dist`));
 // send the user to index html page inspite of the url
